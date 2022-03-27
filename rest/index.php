e54f2287e33946abd0c96db2a46c69eeb53e6054
dorijank <dorijank@localhost.localdomain>
2022-03-27 01:55:43 +0100

    Modifiyed the name of the base dao class

--- rest/index.php
+++ rest/index.php
@@ -1,12 +1,11 @@
 <?php
-echo "dororooror";
-
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require '../vendor/autoload.php';
+require_once 'DAO/BaseDAO.class.php';
 
 
 Flight::route('/', function(){
