acf65b10cc6c5b5542300c20af71fa039abce64d
dorijank <dorijank@localhost.localdomain>
2022-04-14 14:40:22 +0200

    Added get_by_publishing_year method

--- rest/DAO/BooksDAO.class.php
+++ rest/DAO/BooksDAO.class.php
@@ -24,10 +24,34 @@
 
 
         public function get_books_by_writer_name($writer_name,$writer_last_name){
+            
             $stm=$this->get_books_with_writer_names();
-            $stm.=" WHERE w.Writer_Name=':writer_name' AND w.Writer_Last_Name=':writer_last_name'";
+            
+            if($writer_name==null){
+                $stm.=" WHERE w.Writer_Last_Name=':writer_last_name'";
+                $result=$this->conn->prepare($stm);
+                $result->execute(['writer_last_name'=>$writer_last_name]);
+                return @reset($result->fetchAll(PDO::FETCH_ASSOC));
+            }
+            else if($writer_last_name==null){
+                $stm.=" WHERE w.Writer_Name=':writer_name'";
+                $result=$this->conn->prepare($stm);
+                $result->execute(['writer_name'=>$writer_name]);
+                return @reset($result->fetchAll(PDO::FETCH_ASSOC));
+            }
+            else{
+                $stm.=" WHERE w.Writer_Name=':writer_name' OR w.Writer_Last_Name=':writer_last_name'";
+                $result=$this->conn->prepare($stm);
+                $result->execute(['writer_name'=>$writer_name,'writer_last_name'=>$writer_last_name]);
+                return @reset($result->fetchAll(PDO::FETCH_ASSOC));}
+        }
+
+        public function get_by_publishing_year($pub_date){
+            
+            $stm=$this->get_books_with_writer_names();
+            $stm.=" WHERE b.Date_of_Publishing=':pub_date'";
             $result=$this->conn->prepare($stm);
-            $result->execute(['writer_name'=>$writer_name,'writer_last_name'=>$writer_last_name]);
+            $result->execute(['pub_date'=>$pub_date]);
             return @reset($result->fetchAll(PDO::FETCH_ASSOC));
         }
 }
